984e912f1dad482511e9c26675b8f7ff7eb33832
Spencer Evans <github email address>
2017-10-02 16:45:43 -0700

    Done.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,11 +2,8 @@ class LineItemsController < ApplicationController
   #before_filter :authenticate_user!
 
   def create
-    #binding.pry
     initialize_cart
-    #binding.pry
     @line_item = current_cart.add_item(params[:item_id])
-    #binding.pry
     if @line_item.save
       current_cart.save
       redirect_to cart_path(current_cart), alert: "Added Item to Cart!"
